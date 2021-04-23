6fd627662a582acbf451a37f02836f320a4d6ad1
Antoine Chassagne <antoine.chassagne@me.com>
2021-04-23 07:39:39 +0200

    :sparkles: Add information banner component

---
+++ client/components/InformationBanner.vue
@@ -0,0 +1,39 @@
+<template>
+  <div :class="levelClasses">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InformationBanner",
+  props: {
+    level: {
+      type: String,
+      required: true,
+      validator(value) {
+        const levels = ["info", "warning", "error"];
+        if (!levels.includes(value)) {
+          console.log(`Props validation]: Type must be ${levels.join(" or ")}`);
+          return false;
+        }
+        return true;
+      },
+    },
+  },
+  computed: {
+    levelClasses() {
+      switch (this.type) {
+        case "info":
+          return "green";
+        case "warning":
+          return "orange";
+        case "error":
+          return "red";
+        default:
+          return null;
+      }
+    },
+  },
+};
+</script>
