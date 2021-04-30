44edc3a9ef848205b6c13f66aec1e7effd2fbd25
Antoine Chassagne <antoine.chassagne@me.com>
2021-04-30 21:11:49 +0200

    :sparkles: Add custom markers and popups on games map

---
+++ client/components/map/GameMarker.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="marker"></div>
+</template>
+
+<script>
+export default {
+  name: "GameMarker",
+  props: {
+    game: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.marker {
+  width: 25px;
+  height: 25px;
+  background: #667eea;
+  display: block;
+  border-radius: 50%;
+}
+</style>
---
+++ client/components/map/GamePopup.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <Heading level="4" class="mb-2">{{ game.boardGameName }}</Heading>
+    <p class="mb-2">{{ game.description }}</p>
+    <p class="mb-2">
+      Joueurs manquants : <span class="font-bold">{{ game.missingPlayers }}</span>
+    </p>
+    <RouteLink :to="`/games/${game.id}`" :prefetch="false">Voir la partie</RouteLink>
+  </div>
+</template>
+
+<script>
+import Heading from "@/components/texts/Heading";
+
+export default {
+  name: "GamePopup",
+  components: {
+    Heading,
+  },
+  props: {
+    game: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.mapboxgl-popup-content {
+  padding: 1rem;
+  border-radius: 1rem;
+
+  .mapboxgl-popup-close-button {
+    right: 16px;
+    top: 7px;
+  }
+}
+</style>
