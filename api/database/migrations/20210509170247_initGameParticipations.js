6880c902eb426a8f8ba55568e3b13b7942534a1b
Antoine Chassagne <antoine.chassagne@me.com>
2021-05-15 17:29:20 +0200

    :card_file_box: Fix join requests and game participations schemas

--- api/database/migrations/20210509170247_initGameParticipations.js
+++ api/database/migrations/20210509170247_initGameParticipations.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable("gameParticipations", (table) => {
     table.uuid("id").primary();
     table.integer("userId").references("id").inTable("users").notNull();
-    table.integer("gameId").references("id").inTable("games").notNull();
+    table.uuid("gameId").references("id").inTable("games").notNull();
   });
 };
 
