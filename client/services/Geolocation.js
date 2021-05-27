a9ec594c57ccc8d894f9e79587ce02a954ca0759
Antoine Chassagne <antoine.chassagne@me.com>
2021-05-27 15:51:04 +0200

    :recycle: Rework maps

--- client/services/Geolocation.js
+++ client/services/Geolocation.js
@@ -1,4 +1,29 @@
-export function getCurrentGeolocation() {
+export default function () {
+  return new Promise((resolve) => {
+    const result = {
+      supported: isGeolocationSupported(),
+      enabled: true,
+      error: false,
+      location: null,
+    };
+    getCurrentGeolocation()
+      .then(({ latitude, longitude }) => {
+        result.location = { latitude, longitude };
+      })
+      .catch((error) => {
+        if (error.code === 1) {
+          result.enabled = false;
+        } else {
+          result.error = true;
+        }
+      })
+      .finally(() => {
+        resolve(result);
+      });
+  });
+}
+
+function getCurrentGeolocation() {
   return new Promise((resolve, reject) => {
     if (!isGeolocationSupported()) {
       return null;
@@ -19,6 +44,6 @@ export function getCurrentGeolocation() {
   });
 }
 
-export function isGeolocationSupported() {
+function isGeolocationSupported() {
   return "geolocation" in navigator;
 }
