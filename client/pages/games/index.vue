96cb6c0d030803893e9584b40a82a94be8a8c46d
Antoine Chassagne <antoine.chassagne@me.com>
2021-05-28 22:32:48 +0200

    :lipstick: Add basic layout on games search page

--- client/pages/games/index.vue
+++ client/pages/games/index.vue
@@ -1,26 +1,26 @@
 <template>
-  <div>
-    <Heading level="2">Recherchez une partie</Heading>
-    <div class="games">
-      <div class="games__cards">
-        <div v-for="game in games" :key="game.id">
-          {{ game.boardGameName }}
-        </div>
+  <div class="games">
+    <div class="games__cards">
+      <Heading level="2">Recherchez une partie</Heading>
+      <div v-for="game in games" :key="game.id">
+        <CardGame :game="game" class="games__cards__card" />
       </div>
-      <GamesMap :center="location" :games="games" :loading="loading.games" class="games__map" />
     </div>
+    <GamesMap :center="location" :games="games" :loading="loading.games" class="games__map" />
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from "vuex";
 import Heading from "@/components/texts/Heading";
+import CardGame from "@/components/game/CardGame";
 import GamesMap from "@/components/map/GamesMap";
 
 export default {
   name: "PageGames",
   components: {
     Heading,
+    CardGame,
     GamesMap,
   },
   async fetch({ store }) {
@@ -53,11 +53,23 @@ export default {
 .games {
   display: flex;
   flex-wrap: nowrap;
+  overflow: hidden;
+  max-height: calc(100vh - 92px);
+
+  h2 {
+    margin-bottom: 2rem;
+  }
 
   &__cards {
     width: 50%;
     display: flex;
     flex-direction: column;
+    padding: 4rem 2rem 0 2rem;
+    overflow: scroll;
+
+    &__card {
+      margin-bottom: 1rem;
+    }
   }
 
   &__map {
