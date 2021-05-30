a872e204b7dc3d276475372417cffe094a906ee7
Antoine Chassagne <antoine.chassagne@me.com>
2021-05-30 18:57:22 +0200

    :construction: Prepare profil pages

---
+++ client/pages/profil/games.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <Heading level="2">Mes parties</Heading>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import Heading from "@/components/texts/Heading";
+
+export default {
+  name: "PageUserGames",
+  middleware: ["authenticatedOnly"],
+  components: {
+    Heading,
+  },
+  async fetch({ store }) {
+    await store.dispatch("games/fetchGames", {
+      missingPlayers: false,
+    });
+  },
+  computed: {
+    ...mapGetters({
+      user: "authentication/user",
+      games: "games/games",
+    }),
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
